a4a308dcdaaf8699431ba3d51f8f1ec25ef4ad23
will-goodman <58141090+will-goodman@users.noreply.github.com>
2020-04-20 19:03:05 +0100

    Reformatted bootstrap.tf

--- bootstrap.tf
+++ bootstrap.tf
@@ -2,6 +2,6 @@ module "bootstrap" {
   source = "github.com/will-goodman/aws-terraform-modules//bootstrap"
 
   state_bucket_name = "95qded-terraform-state"
-  lock_table_name = "tf-state-lock-table"
+  lock_table_name   = "tf-state-lock-table"
 
 }
