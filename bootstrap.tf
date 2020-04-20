22a017871e0476c37ae8e08a8d2c49d559c242e0
will-goodman <58141090+will-goodman@users.noreply.github.com>
2020-04-20 18:53:07 +0100

    Added bootstrap for terraform

---
+++ bootstrap.tf
@@ -0,0 +1,7 @@
+module "bootstrap" {
+  source = "github.com/will-goodman/aws-terraform-modules//bootstrap"
+
+  state_bucket_name = "95qded-terraform-state"
+  lock_table_name = "tf-state-lock-table"
+
+}
